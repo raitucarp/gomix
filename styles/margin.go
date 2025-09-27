5a3caf9490e50147033458dffa18f069566a5515
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2025-09-27 18:03:59 +0700

    feat(style): add spacing functions style utility

---
+++ styles/margin.go
@@ -0,0 +1,509 @@
+package styles
+
+import "fmt"
+
+func M(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginProp): fmt.Sprintf("calc(var(--spacing) * %d)", number),
+		}
+	}
+}
+
+func NegM(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginProp): fmt.Sprintf("calc(var(--spacing) * -%d)", number),
+		}
+	}
+}
+
+func MAuto() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginProp): "auto",
+		}
+	}
+}
+
+func MPx() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginProp): "1px",
+		}
+	}
+}
+
+func NegMPx() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginProp): "-1px",
+		}
+	}
+}
+
+func MCustomProperty(customProperty string) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginProp): fmt.Sprintf("var(--%s)", customProperty),
+		}
+	}
+}
+
+func MValue(value customValue) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginProp): value.String(),
+		}
+	}
+}
+
+func Mx(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginInlineProp): fmt.Sprintf("calc(var(--spacing) * %d)", number),
+		}
+	}
+}
+
+func NegMx(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginInlineProp): fmt.Sprintf("calc(var(--spacing) * -%d)", number),
+		}
+	}
+}
+
+func MxAuto() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginInlineProp): "auto",
+		}
+	}
+}
+
+func MxPx() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginInlineProp): "1px",
+		}
+	}
+}
+
+func NegMxPx() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginInlineProp): "-1px",
+		}
+	}
+}
+
+func MxCustomProperty(customProperty string) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginInlineProp): fmt.Sprintf("var(--%s)", customProperty),
+		}
+	}
+}
+
+func MxValue(value customValue) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginInlineProp): value.String(),
+		}
+	}
+}
+
+func My(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginBlockProp): fmt.Sprintf("calc(var(--spacing) * %d)", number),
+		}
+	}
+}
+
+func NegMy(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginBlockProp): fmt.Sprintf("calc(var(--spacing) * -%d)", number),
+		}
+	}
+}
+
+func MyAuto() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginBlockProp): "auto",
+		}
+	}
+}
+
+func MyPx() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginBlockProp): "1px",
+		}
+	}
+}
+
+func NegMyPx() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginBlockProp): "-1px",
+		}
+	}
+}
+
+func MyCustomProperty(customProperty string) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginBlockProp): fmt.Sprintf("var(--%s)", customProperty),
+		}
+	}
+}
+
+func MyValue(value customValue) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginBlockProp): value.String(),
+		}
+	}
+}
+
+func Ms(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginInlineStartProp): fmt.Sprintf("calc(var(--spacing) * %d)", number),
+		}
+	}
+}
+
+func NegMs(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginInlineStartProp): fmt.Sprintf("calc(var(--spacing) * -%d)", number),
+		}
+	}
+}
+
+func MsAuto() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginInlineStartProp): "auto",
+		}
+	}
+}
+
+func MsPx() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginInlineStartProp): "1px",
+		}
+	}
+}
+
+func NegMsPx() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginInlineStartProp): "-1px",
+		}
+	}
+}
+
+func MsCustomProperty(customProperty string) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginInlineStartProp): fmt.Sprintf("var(--%s)", customProperty),
+		}
+	}
+}
+
+func MsValue(value customValue) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginInlineStartProp): value.String(),
+		}
+	}
+}
+
+func Me(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginInlineEndProp): fmt.Sprintf("calc(var(--spacing) * %d)", number),
+		}
+	}
+}
+
+func NegMe(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginInlineEndProp): fmt.Sprintf("calc(var(--spacing) * -%d)", number),
+		}
+	}
+}
+
+func MeAuto() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginInlineEndProp): "auto",
+		}
+	}
+}
+
+func MePx() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginInlineEndProp): "1px",
+		}
+	}
+}
+
+func NegMePx() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginInlineEndProp): "-1px",
+		}
+	}
+}
+
+func MeCustomProperty(customProperty string) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginInlineEndProp): fmt.Sprintf("var(--%s)", customProperty),
+		}
+	}
+}
+
+func MeValue(value customValue) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginInlineEndProp): value.String(),
+		}
+	}
+}
+
+func Mt(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginTopProp): fmt.Sprintf("calc(var(--spacing) * %d)", number),
+		}
+	}
+}
+
+func NegMt(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginTopProp): fmt.Sprintf("calc(var(--spacing) * -%d)", number),
+		}
+	}
+}
+
+func MtAuto() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginTopProp): "auto",
+		}
+	}
+}
+
+func MtPx() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginTopProp): "1px",
+		}
+	}
+}
+
+func NegMtPx() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginTopProp): "-1px",
+		}
+	}
+}
+
+func MtCustomProperty(customProperty string) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginTopProp): fmt.Sprintf("var(--%s)", customProperty),
+		}
+	}
+}
+
+func MtValue(value customValue) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginTopProp): value.String(),
+		}
+	}
+}
+
+func Mr(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginRightProp): fmt.Sprintf("calc(var(--spacing) * %d)", number),
+		}
+	}
+}
+
+func NegMr(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginRightProp): fmt.Sprintf("calc(var(--spacing) * -%d)", number),
+		}
+	}
+}
+
+func MrAuto() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginRightProp): "auto",
+		}
+	}
+}
+
+func MrPx() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginRightProp): "1px",
+		}
+	}
+}
+
+func NegMrPx() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginRightProp): "-1px",
+		}
+	}
+}
+
+func MrCustomProperty(customProperty string) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginRightProp): fmt.Sprintf("var(--%s)", customProperty),
+		}
+	}
+}
+
+func MrValue(value customValue) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginRightProp): value.String(),
+		}
+	}
+}
+
+func Mb(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginBottomProp): fmt.Sprintf("calc(var(--spacing) * %d)", number),
+		}
+	}
+}
+
+func NegMb(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginBottomProp): fmt.Sprintf("calc(var(--spacing) * -%d)", number),
+		}
+	}
+}
+
+func MbAuto() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginBottomProp): "auto",
+		}
+	}
+}
+
+func MbPx() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginBottomProp): "1px",
+		}
+	}
+}
+
+func NegMbPx() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginBottomProp): "-1px",
+		}
+	}
+}
+
+func MbCustomProperty(customProperty string) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginBottomProp): fmt.Sprintf("var(--%s)", customProperty),
+		}
+	}
+}
+
+func MbValue(value customValue) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginBottomProp): value.String(),
+		}
+	}
+}
+
+func Ml(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginLeftProp): fmt.Sprintf("calc(var(--spacing) * %d)", number),
+		}
+	}
+}
+
+func NegMl(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginLeftProp): fmt.Sprintf("calc(var(--spacing) * -%d)", number),
+		}
+	}
+}
+
+func MlAuto() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginLeftProp): "auto",
+		}
+	}
+}
+
+func MlPx() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginLeftProp): "1px",
+		}
+	}
+}
+
+func NegMlPx() ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginLeftProp): "-1px",
+		}
+	}
+}
+
+func MlCustomProperty(customProperty string) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginLeftProp): fmt.Sprintf("var(--%s)", customProperty),
+		}
+	}
+}
+
+func MlValue(value customValue) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(marginLeftProp): value.String(),
+		}
+	}
+}
+
+// TODO: Space-X and Space-Y
---
+++ styles/padding.go
@@ -0,0 +1,291 @@
+package styles
+
+import "fmt"
+
+func P(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingProp): fmt.Sprintf("calc(var(--spacing) * %d)", number),
+		}
+	}
+}
+
+func PPx(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingProp): "1px",
+		}
+	}
+}
+
+func PCustomProperty(customProperty string) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingProp): fmt.Sprintf("var(--%s)", customProperty),
+		}
+	}
+}
+
+func PValue(value customValue) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingProp): value.String(),
+		}
+	}
+}
+
+func Px(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingInlineProp): fmt.Sprintf("calc(var(--spacing) * %d)", number),
+		}
+	}
+}
+
+func PxPx(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingInlineProp): "1px",
+		}
+	}
+}
+
+func PxCustomProperty(customProperty string) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingInlineProp): fmt.Sprintf("var(--%s)", customProperty),
+		}
+	}
+}
+
+func PxValue(value customValue) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingInlineProp): value.String(),
+		}
+	}
+}
+
+func Py(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingBlockProp): fmt.Sprintf("calc(var(--spacing) * %d)", number),
+		}
+	}
+}
+
+func PyPx(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingBlockProp): "1px",
+		}
+	}
+}
+
+func PyCustomProperty(customProperty string) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingBlockProp): fmt.Sprintf("var(--%s)", customProperty),
+		}
+	}
+}
+
+func PyValue(value customValue) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingBlockProp): value.String(),
+		}
+	}
+}
+
+func Ps(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingInlineStartProp): fmt.Sprintf("calc(var(--spacing) * %d)", number),
+		}
+	}
+}
+
+func PsPx(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingInlineStartProp): "1px",
+		}
+	}
+}
+
+func PsCustomProperty(customProperty string) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingInlineStartProp): fmt.Sprintf("var(--%s)", customProperty),
+		}
+	}
+}
+
+func PsValue(value customValue) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingInlineStartProp): value.String(),
+		}
+	}
+}
+
+func Pe(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingInlineEndProp): fmt.Sprintf("calc(var(--spacing) * %d)", number),
+		}
+	}
+}
+
+func PePx(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingInlineEndProp): "1px",
+		}
+	}
+}
+
+func PeCustomProperty(customProperty string) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingInlineEndProp): fmt.Sprintf("var(--%s)", customProperty),
+		}
+	}
+}
+
+func PeValue(value customValue) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingInlineEndProp): value.String(),
+		}
+	}
+}
+
+func Pt(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingTopProp): fmt.Sprintf("calc(var(--spacing) * %d)", number),
+		}
+	}
+}
+
+func PtPx(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingTopProp): "1px",
+		}
+	}
+}
+
+func PtCustomProperty(customProperty string) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingTopProp): fmt.Sprintf("var(--%s)", customProperty),
+		}
+	}
+}
+
+func PtValue(value customValue) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingTopProp): value.String(),
+		}
+	}
+}
+
+func Pr(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingRightProp): fmt.Sprintf("calc(var(--spacing) * %d)", number),
+		}
+	}
+}
+
+func PrPx(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingRightProp): "1px",
+		}
+	}
+}
+
+func PrCustomProperty(customProperty string) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingRightProp): fmt.Sprintf("var(--%s)", customProperty),
+		}
+	}
+}
+
+func PrValue(value customValue) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingRightProp): value.String(),
+		}
+	}
+}
+
+func Pb(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingBottomProp): fmt.Sprintf("calc(var(--spacing) * %d)", number),
+		}
+	}
+}
+
+func PbPx(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingBottomProp): "1px",
+		}
+	}
+}
+
+func PbCustomProperty(customProperty string) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingBottomProp): fmt.Sprintf("var(--%s)", customProperty),
+		}
+	}
+}
+
+func PbValue(value customValue) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingBottomProp): value.String(),
+		}
+	}
+}
+
+func Pl(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingLeftProp): fmt.Sprintf("calc(var(--spacing) * %d)", number),
+		}
+	}
+}
+
+func PlPx(number int) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingLeftProp): "1px",
+		}
+	}
+}
+
+func PlCustomProperty(customProperty string) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingLeftProp): fmt.Sprintf("var(--%s)", customProperty),
+		}
+	}
+}
+
+func PlValue(value customValue) ApplyProp {
+	return func(s *style) styleProp {
+		return &properties{
+			string(paddingLeftProp): value.String(),
+		}
+	}
+}
