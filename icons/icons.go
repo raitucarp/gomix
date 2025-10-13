be87bab16fb4c3acf52679c0f00bdeb1edbca891
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2025-10-13 16:52:15 +0700

    feat(icons): add shared function to read svg file

--- icons/icons.go
+++ icons/icons.go
@@ -1,8 +1,32 @@
 package icons
 
-import "github.com/raitucarp/gomix/element"
+import (
+	"embed"
+	"path"
+
+	"github.com/raitucarp/gomix/element"
+)
 
 type IsIcon interface {
 	IsIcon()
 	Element() *element.HtmlElement
+	Name() string
+}
+
+func readSvgIcon(fs embed.FS, svgPaths ...string) (string, error) {
+	f, err := fs.ReadFile(path.Join(svgPaths...) + ".svg")
+	if err != nil {
+		return "", err
+	}
+
+	return string(f), err
+}
+
+func CreateSvgElementByFs(fs embed.FS, paths ...string) *element.HtmlElement {
+	f, err := readSvgIcon(fs, paths...)
+	if err != nil {
+		panic(err)
+	}
+	svgEl, _ := element.SvgString(f)
+	return svgEl.Element()
 }
